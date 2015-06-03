7f14104012b94f77eca0941f3907509f06b1ea92
Jason Studstill <standstill64@gmail.com>
2015-06-03 01:47:13 -0700

    Complete all problems - INCOMPLETE: Problem 2 assertions don't check out

--- lab2.js
+++ lab2.js
@@ -90,6 +90,7 @@ Blob.prototype.hoursToOoze = hoursToOoze;
 assert(blob.hoursToOoze(0, 1) === 0, "no people means no time needed.");
 assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
   "hoursSpentInDowington should match hoursToOoze\"s result for 1000");
+
 // TODO: write three more assertions like the two above, testing out
 // the hoursToOoze method.
 
@@ -103,45 +104,45 @@ var hello = {
   "federation standard": "hello" // home planet is Earth
 };
 
-// TODO: define a constructor that creates objects to represent
+// define a constructor that creates objects to represent
 // sentient beings. They have a home planet, a language that they
 // speak, and method called sayHello.
 
 function SentientBeing (homePlanet, language) {
   this.homePlanet = homePlanet;
   this.language = language;
-  this.hello = "I have no default greeting";
-  this.sayHello = function sayHello(sb) {
-    sb = this;
-    conosle.log(this.hello);
+  this.hello = hello[language];
+  this.sayHello = function(sb) {
+    console.log(this.hello);
     return sb.hello;
   }
-
-  // TODO: specify a home planet and a language
-  // you'll need to add parameters to this constructor
 }
 
-// sb is a SentientBeing object
-function sayHello (sb) {
-    // TODO: say hello prints out (console.log's) hello in the
-    // language of the speaker, but returns it in the language
-    // of the listener (the sb parameter above).
-    // use the 'hello' object at the beginning of this exercise
-    // to do the translating
+var klingon = new SentientBeing('Qo"noS', "klingon");
+var romulan = new SentientBeing("Romulus", "romulan");
+var Human = new SentientBeing("Earth", "federation standard");
 
-    //TODO: put this on the SentientBeing prototype
-  }
+function Klingon() {}
+function Romulan() {}
+function Human() {}
+Human.prototype = new SentientBeing("Earth", "federation standard")
+Klingon.prototype = new SentientBeing('Qo"noS', "klingon");
+Romulan.prototype = new SentientBeing("Romulus", "romulan");
 
-// TODO: create three SentientBeings, one for each language in the
-// 'hello' object above.
-var klingon = new SentientBeing(); // TODO: fix me
-var romulan = new SentientBeing(); // TODO: fix me
-var human = new SentientBeing(); // TODO: fix me
 
 assert((new Human()).sayHello(new Klingon()) === "nuqneH",
   "the klingon should hear nuqneH");
-// TODO: write five more assertions, to complete all the possible
-// greetings between the three types of sentient beings you created above.
+
+assert((new Human()).sayHello(new Romulan()) === "Jolan\"tru",
+  "the romulan should hear Jolan\"tru");
+assert((new Romulan()).sayHello(new Klingon()) === "nuqneH",
+  "the klingon should hear nuqneH");
+assert((new Romulan()).sayHello(new Human()) === "hello",
+  "the human should hear hello");
+assert((new Klingon()).sayHello(new Romulan()) === "Jolan\"tru",
+  "the romulan should hear Jolan\"tru");
+assert((new Klingon()).sayHello(new Human()) === "hello",
+  "the human should hear hello");
 
 //*********************************************************
 // PROBLEM 3: Moar Loops. 20 points.
@@ -150,26 +151,53 @@ assert((new Human()).sayHello(new Klingon()) === "nuqneH",
 // assertions for each one
 //*********************************************************
 function max(array) {
-  // TODO: return the largest number in the given array
+  // return the largest number in the given array
+  var newMax = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > newMax) {
+      newMax = array[i];
+    }
+  }
+  return newMax;
 }
 
-// TODO: write three more assertions
 assert(max([ 1, 3, 2 ]) === 3, "[1,3,2]");
+assert(max([ 1, 5, 7, 9]) === 9, "[1,5,7,9]");
+assert(max([1]) === 1, "[1]");
+assert(max([ 2.33, 9.9999, 100.255, 6 ]) === 100.255, '[2.33,9.9999,100.255,6 ]');
+
 
 function variablify(string) {
-  // TODO: you are given a string with several words in it
-  // return a corresponding variable name that follows
-  // javascript conventions
-  // HINT:
-  // you might want to use these string methods:
-  //  split(), charAt(), toUpperCase()
-  // and this array method: join()
+  var array = string.split(' ');
+  var camelString = '';
+  for (var i = 0; i < array.length; i++) {
+    if (i === 0) {
+      var firstWord = array[i];
+      camelString += firstWord.toLowerCase();
+    } else {
+      var nextWord = array[i];
+      camelString += nextWord.charAt(0).toUpperCase();
+      for (var j = 1; j < nextWord.length; j++) {
+        camelString += nextWord.charAt(j).toLowerCase();
+      }
+    }
+  }
+  return camelString;
 }
+
 
 // TODO: write three more assertions
 assert(variablify("one two three") === "oneTwoThree",
   "variablify(\"one two three\")");
 
+assert(variablify("This is my String") === "thisIsMyString",
+  "variablify(\"This is my String\") should return 'thisIsMyString'");
+
+assert(variablify("NEW VARIABLE") === "newVariable",
+  "variablify(\"NEW VARIABLE\") should return 'newVariable'");
+
+assert(variablify("CrAzY CASe") === "crazyCase",
+  "variablify(\"CrAzY CASe\") should return 'crazyCase'");
 //*********************************************************
 // PROBLEM 4: Cleanup: 10 points
 // Makes sure this file passes jshint and jscs
